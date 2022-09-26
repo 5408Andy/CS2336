e46390e7ad6a76526feec771b603dfdc6a7f4f67
Andy Nguyen <anry2805@gmail.com>
2022-09-26 17:29:01 -0500

    In Class Assignment #2
    Test run of the IC2

--- Class/IC2/Document.java
+++ Class/IC2/Document.java
@@ -1,5 +1,27 @@
 public class Document {
 
+    protected String textualContent;
 
+    Document() { textualContent = ""; } // constructor
+
+    Document(String textReceived) { textualContent = textReceived; } // constructor
+
+    public void setTextualContent(String textReceived) { // mutater
+
+        textualContent = textReceived;
+
+    }
+
+    public String getTextualContent() { // accessor
+
+        return textualContent;
+
+    }
+
+    public String toString() {
+
+        return "Textual Content: " + textualContent;
+
+    }
 
 }
--- Class/IC2/Email.java
+++ Class/IC2/Email.java
@@ -1,3 +1,69 @@
-public class Email {
+public class Email extends Document {
     
+    protected String sender;
+    protected String recipient;
+    protected String title;
+
+    Email() {
+
+        super();
+
+        sender = "";
+        recipient = "";
+        title = "";
+
+    }
+
+    Email(String senderReceived, String recipientReceived, String titleRecieved, String textReceived) {
+
+        super(textReceived);
+
+        sender = "";
+        recipient = "";
+        title = "";
+
+    }
+
+    public void setSender(String senderReceived) {
+
+        sender = senderReceived;
+
+    }
+
+    public String getSender() {
+
+        return sender;
+
+    }
+
+    public void setRecipient(String recipientReceived) {
+
+        recipient = recipientReceived;
+
+    }
+
+    public String getRecipient() {
+
+        return recipient;
+
+    }
+
+    public void setTitle(String titleRecieved) {
+
+        title = titleRecieved;
+
+    }
+
+    public String getTitle() {
+
+        return title;
+
+    }
+
+    public String toString() {
+
+        return "Sender: " + sender + " Recipient: " + recipient + " Title: " + title + " Textual Content: " + super.textualContent;
+
+    }
+
 }
