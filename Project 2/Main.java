f2d54fa9527dfd7d24e95fec9bda0433fcab8ad2
Andy Nguyen <anry2805@gmail.com>
2022-10-01 19:45:34 -0500

    More Project 2

--- Project 2/Main.java
+++ Project 2/Main.java
@@ -17,35 +17,21 @@ public class Main {
 
         LinkedList playerList = new LinkedList();
         
-        //String fileName = askInputFileName();
+        // get the input file for processing
+        String fileName = askInputFileName();
 
-        //File inputFile = new File(fileName); 
-        //FileInputStream inputFileStream = new FileInputStream(inputFile);
-        //Scanner scanFileLine = new Scanner(inputFile);
+        // file scanning and processing
+        File inputFile = new File(fileName); 
+        FileInputStream inputFileStream = new FileInputStream(inputFile);
+        Scanner scanFileLine = new Scanner(inputFile);
 
         System.out.println(); // create an empty line
         
-        /* 
         if (inputFile.exists() == true) { // makes sure the input file actually exists
 
             while (scanFileLine.hasNextLine() == true) { // keeps scanning file until it reaches the end of the file
 
-                readFileLine(scanFileLine, );
-
-                if (playerNameArray[playerIndex] == null) { // skips blank lines
-
-                    continue;
-        
-                }
-
-                parsePlayerStats(playerHitStat, playerOutStat, playerStrikeOutStat, playerWalkStat, playerHitByPitchStat, playerSacrificeStat, playerStatsArray[playerIndex], playerIndex);
-
-                calculateBattingAverage(playerStrikeOutStat, playerOutStat, playerHitStat, playerIndex);
-
-                battingAverageStat[playerIndex] = calculateBattingAverage(playerStrikeOutStat, playerOutStat, playerHitStat, playerIndex);
-                onBasePercentageStat[playerIndex] = calculateOnBasePercentage(playerHitStat, playerWalkStat, playerHitByPitchStat, playerOutStat, playerStrikeOutStat, playerSacrificeStat, playerIndex);
-
-                playerIndex++;
+                readFileLine(scanFileLine, playerList);
 
             }
 
@@ -55,20 +41,12 @@ public class Main {
             System.out.println("\"" + fileName + "\" could not be opened!");
 
         }
-        */
-
+        
         // Closes the input file and the scanner
-        //inputFileStream.close();
-        //scanFileLine.close();
+        inputFileStream.close();
+        scanFileLine.close();
 
         // - - - Testing - - - //
-        
-        playerList.appendPlayer(new Player("Bob", "WTFHOWDOESTHISWORK"));
-        playerList.appendPlayer(new Player("Tom", "ASDASD"));
-        playerList.appendPlayer(new Player("Zed", "WasdSTHISWORK"));
-        playerList.appendPlayer(new Player("Ada", "WTFHOWDOESTHISWORK"));
-        playerList.appendPlayer(new Player("Aba", "WTFHOWDOESTHISWORK"));
-        playerList.appendPlayer(new Player("Jane", "WTFHOWDOESTHISWORK"));
 
         System.out.println("H1");
         System.out.println(playerList.checkListInOrder());
@@ -80,26 +58,6 @@ public class Main {
         System.out.println(playerList.checkListInOrder());
         playerList.printStatsRecursively(playerList.getHeadNode());
         
-        
-        /* 
-        playerList.sortPlayers();
-
-        System.out.println("\nH3");
-        //System.out.println(playerList.checkListInOrder());
-        playerList.printStatsRecursively(playerList.getHeadNode());
-
-        playerList.sortPlayers();
-
-        System.out.println("\nH4");
-        //System.out.println(playerList.checkListInOrder());
-        playerList.printStatsRecursively(playerList.getHeadNode());
-        //System.out.println();
-
-        //playerList.printStatsRecursively(playerList.getHeadNode());
-        */
-        
-        //playerList.TEMPORARY_printStats();
-
     } // Main
 
     public static String askInputFileName() {
@@ -116,6 +74,34 @@ public class Main {
 
     } // askInputFileName
 
-    //public static void readFileLine(Scanner scanFileLine, )
+    public static void readFileLine(Scanner scanFileLine, LinkedList playerList) {
+
+        // strings to store information from file
+        String fileLine = new String();
+        String playerName = new String();
+        String playerStats = new String();
+        
+        fileLine = scanFileLine.nextLine(); // scans line in the file
+
+        if (fileLine.length() != 0) { // does not process blank lines
+
+            for (int fileLineIndex = 0; fileLineIndex < fileLine.length(); fileLineIndex++) {
+
+                if(fileLine.charAt(fileLineIndex) == ' ') { 
+
+                    // using substring method to seperate names and stats
+                    playerName = fileLine.substring(0, fileLineIndex);
+                    playerStats = fileLine.substring(fileLineIndex + 1, fileLine.length());
+                
+                }
+
+            }
+
+            // stores extracted names and stats into player class
+            playerList.appendPlayer(new Player(playerName, playerStats));
+
+        }
+
+    } // readFileLine
 
 }
